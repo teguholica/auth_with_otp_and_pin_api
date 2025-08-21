3473815fb73add62331f5e4b953a0e65a2b87a5e
Teguh Arifianto <teguholica@gmail.com>
2025-08-21 08:21:00 +0700

    initial commit

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  testEnvironment: "node",
+  coverageDirectory: "coverage",
+  collectCoverageFrom: [
+    "src/**/*.js",
+    "!src/server.js",      // exclude entrypoint
+    "!src/app.js"          // exclude bootstrap file
+  ],
+  coverageThreshold: {
+    global: {
+      statements: 90,
+      branches: 80,
+      functions: 90,
+      lines: 90,
+    },
+  },
+};
